9c5cea8b8a1a4fd79601d4a4afac79b232ddc059
nikolaj1307 <nikolaj1307@live.dk>
2024-01-18 11:13:14 +0100

    Add category fxml + update on addMovieView

--- src/BLL/CategoryManager.java
+++ src/BLL/CategoryManager.java
@@ -32,4 +32,6 @@ public class CategoryManager {
             throw new MovieExceptions(e);
         }
     }
+
+
 }
---
+++ src/GUI/Controller/CategoryAddRemoveViewController.java
@@ -0,0 +1,24 @@
+package GUI.Controller;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CategoryAddRemoveViewController {
+
+    @FXML
+    private MFXButton categoryCancelBtn;
+
+    @FXML
+    private MFXButton categorySaveBtn;
+
+    @FXML
+    private TextField categoryTextField;
+
+    public void onClickCategoryCancelBtn(ActionEvent event) {
+    }
+
+    public void onClickCategorySaveBtn(ActionEvent event) {
+    }
+}
